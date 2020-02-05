00e8fa3920b4987bc5f97f74c3c6fa86e04743c5
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-02-06 02:40:19 +0800

    Update Exercise 9.1.c

--- chapter09/Exercise 9.1.c
+++ chapter09/Exercise 9.1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+double imin(double, double);
+
+int main(void)
+{
+    double num_1, num_2;
+
+    printf("Enter two numbers of type double:");
+    scanf("%lf %lf", &num_1, &num_2);
+    printf("The smaller value: %lf", imin(num_1, num_2));
+
+    return 0;
+}
+
+double imin(double num_1, double num_2)
+{
+    return num_1 > num_2 ? num_2 : num_1; //USing if else statemen can achieve the same function
+}
