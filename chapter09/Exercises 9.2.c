df8db1fd6ca2c7e5b0240e3cde13858d9045c851
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-02-07 23:30:50 +0800

    Update Exercises 9.2.c

--- chapter09/Exercises 9.2.c
+++ chapter09/Exercises 9.2.c
@@ -1,1 +1,31 @@
-# include <stdio.h>
+#include <stdio.h>
+
+void ch_line(char ch, int column, int line); 
+
+int main(void)
+{
+    int ch, column, line;
+    printf("Enter the char want to print:");
+
+    ch = getchar();
+    while (getchar() != '\n')
+        continue;
+
+    printf("Enter how many column and line want to print(Separate the two numbers with a space):");
+    scanf("%d %d", &column, &line);
+
+    ch_line(ch, column, line);
+
+    return 0;
+}
+
+void ch_line(char ch, int column, int line)
+{
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+            printf("%c", ch);
+
+        printf("\n");
+    }
+}
