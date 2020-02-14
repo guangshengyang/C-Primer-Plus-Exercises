ae91c51611a3f604bca846534ea2b345c2e786d7
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-02-14 23:45:40 +0800

    Update Exercises 10.01.c

--- chapter10/Exercises 10.01.c
+++ chapter10/Exercises 10.01.c
@@ -4,19 +4,33 @@
 
 int main(void)
 {
-    const float rain[YEARS][MONTHS] =
+     float rain[YEARS][MONTHS] =
         {{4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
          {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
          {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
          {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
          {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6}};
+
     int year, month;
     float subtotal, total;
+
+    
     printf(" YEAR    RAINFALL   (inches)\n");
     for (int year = 0; year < YEARS; year++)
     {
-        for (int month = 0; month < MONTHS; month++)
+        subtotal = *rain++;
+    }
+    printf("\nThe yearly average is %.1f inches.\n", subtotal / YEARS);
+
+    printf("MONTH    RAINFALL   (inches)\n");
+
+    for (int month = 0; month < MONTHS; month++)
+    {
+        for (int year = 0; year < YEARS; year++)
         {
+            total = *rain++;
         }
     }
+
+    return 0;
 }
