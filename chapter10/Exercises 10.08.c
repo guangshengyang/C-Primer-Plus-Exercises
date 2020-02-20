2c3a40fbc2a3843138d9b625cc377769b09419a5
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-02-20 14:49:06 +0800

    Update Function's parameters
    Use the function form parameters that better meet the requirements of the problem according to the requirements of the problem

--- chapter10/Exercises 10.08.c
+++ chapter10/Exercises 10.08.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-void copyarray(double *ch_another, double *ch_start, double *ch_end);
+void copyarray(double *ch_another, double *ch_start, int number); //According to the requirements of the title,
+                                                                  //the formal parameters are
+                                                                  //the address of the array element and the number of elements
 void printout_array(double *ch, double *ch_another);
 
 int main(void)
@@ -8,16 +10,17 @@ int main(void)
     double ch[7] = {1, 2, 3, 4, 5, 6, 7};
     double ch_another[3];
 
-    copyarray(ch_another, ch + 2, ch + 5);
+    copyarray(ch_another, ch + 2, 3);
     printout_array(ch, ch_another);
 
     return 0;
 }
 
-void copyarray(double *ch_another, double *ch_start, double *ch_end)
+void copyarray(double *ch_another, double *ch_start, int number)
 {
-    while (ch_start < ch_end)
+    do
         *ch_another++ = *ch_start++;
+    while (number-- > 0);
 }
 
 void printout_array(double *ch, double *ch_another)
@@ -28,5 +31,5 @@ void printout_array(double *ch, double *ch_another)
 
     printf("\nTarget Array:\n");
     for (int i = 0; i < 3; i++)
-        printf("%g\t", *(ch_another + i));//equal to print("%g",ch_another[i]);
+        printf("%g\t", *(ch_another + i)); //equal to print("%g",ch_another[i]);
 }
