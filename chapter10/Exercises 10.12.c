55640eab96d18b1fdf2aa8048571369b909d95ed
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-02-25 00:59:50 +0800

    Update Exercises 10.12.c

--- chapter10/Exercises 10.12.c
+++ chapter10/Exercises 10.12.c
@@ -1,6 +1,51 @@
-#include<stdio.h>
 
-int main()
+#include <stdio.h>
+#define MONTHS 12
+#define YEARS 5
+
+void YearAverageRainfall(float (*rain)[MONTHS]);
+void MonthAverageRainfall(float (*rain)[MONTHS]);
+
+int main(void)
 {
+    float rain[YEARS][MONTHS] = {
+        {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
+        {8.5, 8.2, 1.2, 1.6, 2.4, 0.0, 5.2, 0.9, 0.3, 0.9, 1.4, 7.3},
+        {9.1, 8.5, 6.7, 4.3, 2.1, 0.8, 0.2, 0.2, 1.1, 2.3, 6.1, 8.4},
+        {7.2, 9.9, 8.4, 3.3, 1.2, 0.8, 0.4, 0.0, 0.6, 1.7, 4.3, 6.2},
+        {7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2}};
+
+    YearAverageRainfall(rain);
+    MonthAverageRainfall(rain);
+
     return 0;
+}
+
+void YearAverageRainfall(float (*rain)[MONTHS])
+{
+    float subtotal, total;
+    printf(" YEAR    RAINFALL   (inches)\n");
+    for (int year = 0; year < YEARS; year++)
+    {
+        for (int month = 0; month < MONTHS; month++)
+            subtotal += *(*(rain + year) + month);
+        printf("%5d %20.1f\n", 2010 + year, subtotal);
+        total += subtotal; // total for all years
+    }
+    printf("\nThe yearly average is %.1f inches.\n", subtotal / YEARS);
+}
+
+void MonthAverageRainfall(float (*rain)[MONTHS])
+{
+    float subtotal, total;
+
+    printf("\n Jan    Feb    Mar    Apr    May    Jun     Jul     Aug     Sep     Oct     Nov     Dec\n");
+    for (int month = 0; month < MONTHS; month++)
+    {
+        for (int year = 0; year < YEARS; year++)
+            subtotal += *(*(rain + month) + year);
+        printf("%.1f  ", subtotal);
+        total += subtotal; // total for all months
+    }
+    printf("\n\nThe monthly average is %.1f inches.\n", subtotal / YEARS);
 }
