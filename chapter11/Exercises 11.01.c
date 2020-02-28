3c478edb28ebb36d33d9247ce2744a4906a29e1f
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-02-28 18:08:54 +0800

    Update Exercises 11.01.c

--- chapter11/Exercises 11.01.c
+++ chapter11/Exercises 11.01.c
@@ -1,4 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#define N 10 
 
-int main(viod)
-  
+void PassAdress(char *ch);
+
+int main(void)
+{
+    char ch[N];
+
+    PassAdress(ch);
+
+    for (int i = 0; i < N; i++)
+        putchar(*(ch + i));
+
+    return 0;
+}
+
+void PassAdress(char *ch)
+{
+    int chch;
+    printf("Enter any chars is OK:\n");
+    while ((chch = getchar()) != EOF)
+        *ch++ = chch;
+}
