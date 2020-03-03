612d73c8dfdf2bad855e7444972c542519d95ec7
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-03 20:39:51 +0800

    Update Exercises 11.05.c

--- chapter11/Exercises 11.05.c
+++ chapter11/Exercises 11.05.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <string.h>
 
+char *MyStrchr(const char *s, char c);
+
+int main(void)
+{
+    char ch;
+    char *return_ptr;
+    char source_string[] = "Welcome to VS Code!";
+    printf("What character you are look up?\n");
+    ch = getchar();
+
+    return_ptr = MyStrchr(source_string, ch);
+
+    printf("Pointer to the first position of the source string:%p\n"
+           "The Return pointer:%p",
+           source_string, return_ptr);
+
+    return 0;
+}
+
+char *MyStrchr(const char *s, char c)
+{
+    int tempch;
+    const char *initial_pointer = s;
+    for (int i = 0; i < strlen(s) + 1; i++) // "\0" is part of the string
+    {
+        tempch = *s++;
+        if (tempch == c)
+        {
+            return initial_pointer;
+        }
+        continue;
+    }
+    return NULL;
+}
