25ab22a513808f3567bddd877546d0f2b50f642e
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-05 23:10:22 +0800

    Update Exercises 11.08.c

--- chapter11/Exercises 11.08.c
+++ chapter11/Exercises 11.08.c
@@ -1,2 +1,37 @@
+#include <stdio.h>
+#include <string.h>
 
-#include
+char string_in(char *s1, char *s2);
+
+int main(void)
+{
+    char s1[] = "hats";
+    char s2[] = "at";
+    printf("%p\n%p\n", s1, s2);
+    string_in(s1, s2);
+    printf("%p", string_in(s1, s2));
+    return 0;
+}
+
+char string_in(char *s1, char *s2)
+{
+    char *ret_s1;
+    while (*s1++ == *s2)
+    {
+        ret_s1 = s1--;
+        printf("%p\n", ret_s1);
+        for (int i = 0; i < strlen(s2); i++)
+        {
+            if (*s1++ == *s2++)
+            {
+                continue;
+            }
+            else
+            {
+                return NULL;
+            }
+        }
+        return ret_s1;
+    }
+    return NULL;
+}
