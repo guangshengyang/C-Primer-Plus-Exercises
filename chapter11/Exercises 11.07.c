3a08c3ae2b080ff5926356d330ade4bcafb7c3ae
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-05 22:39:17 +0800

    Update Exercises 11.07.c
    Main code is here  
     while (n--)
            {
                *s1++ = *s2++;
            }

--- chapter11/Exercises 11.07.c
+++ chapter11/Exercises 11.07.c
@@ -1,38 +1,59 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 30
-#define N 10
+#define SIZE 40
 
+char Getn(char n);
 char *mystrncpy(char *s1, char *s2, int n);
 
 int main(void)
 {
-    char s1[SIZE] = "Welcome to VS Code";
-    char s2[] = " with many extentions";
-    int n;
+    char n;
+    char s1[SIZE] = "Welcome to VS Codecode";
+    char s2[] = "with extentions";
 
-    printf("How many characters you want to copy to s1?\n");
-    n = getchar();
-    while (getchar() != '\n')
-        continue;
-        
-    printf("s1 pointer:%p\n", s1);
-    char ret_ptr = mystrncpy(s1, s2, n);
+    n = Getn(n);
+    mystrncpy(s1, s2, n);
 
-    printf("Return pointer:%p\n", ret_ptr);
-    printf("%d\n", s1 - ret_ptr);
+    puts(s1);
 
     return 0;
 }
 
+char Getn(char n)
+{
+    printf("How many characters you want to copy to s1?\n");
+    n = getchar();
+    while (getchar() != '\n')
+        continue;
+    return n;
+}
 char *mystrncpy(char *s1, char *s2, int n)
 {
-    if (*s1++ = '\0')
+    char substitute_n = n;
+    char *substitute_s1 = s1;
+    if (strlen(s2) < n)
     {
-        for (int i = 0; i < SIZE - strlen(s1); i++)
+        char len = strlen(s2);
+        while (len--)
         {
-            *s1++ = *s2;
+            *s1++ = *s2++;
         }
-        return s1;
+        *(s1) = '\0';
+        puts("Warning：copying may end early because n is greater than the length of array s1");
+        return substitute_s1;
     }
+    else
+    {
+        while (n--)
+        {
+            *s1++ = *s2++;
+        }
+
+        if (n > SIZE)
+        {
+            printf("Warring: The target array s1 is not long enough to hold %d length strings\n", substitute_n);
+        }
+    }
+
+    return substitute_s1;
 }
