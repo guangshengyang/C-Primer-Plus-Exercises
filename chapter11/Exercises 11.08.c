e63ab30ad9158bb6f16fedb094aadadc8c516885
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-06 21:40:03 +0800

    Update Exercises 11.08.c

--- chapter11/Exercises 11.08.c
+++ chapter11/Exercises 11.08.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#define MAXSIZE 30
 
+void GetString(char *s1, char *s2);
+void ClearCR(char *s);
 char *string_in(char *s1, char *s2);
 
 int main(void)
 {
-    char s1[] = "hats";
-    char s2[] = "at";
+    char s1[MAXSIZE];
+    char s2[MAXSIZE];
 
+    GetString(s1, s2);
     printf("The beggin of string s1 address:%p\n", s1);
     printf("The returned address：%p", string_in(s1, s2));
 
     return 0;
+}
+
+void GetString(char *s1, char *s2)
+{
+    puts("The program target is to find if string \"s2\" is in string \"s1\"\n");
+
+    puts("Now enter string s1 next line:");
+
+    fgets(s1, MAXSIZE, stdin); //fgets() store the CR(carriage return) in the array
+    ClearCR(s1);               //CR need to replace by '\0',or program will meet bugs
+
+    puts("Now enter s2 next line:");
+
+    fgets(s2, MAXSIZE, stdin);
+    ClearCR(s2);
+}
+
+void ClearCR(char *s)
+{
+    int i = 0;
+    while (s[i] != '\n')
+        s++;
+
+    s[i] = '\0';
 }
 
 char *string_in(char *s1, char *s2)
