28f749cb27040d47e609725f6669143b11ef75dc
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-06 23:21:56 +0800

    Update Exercises 11.10.c

--- chapter11/Exercises 11.10.c
+++ chapter11/Exercises 11.10.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 30
+
+void RemoveSpace_inString(char *s);
+int main(void)
+{
+    char s[SIZE];
+
+    while (fgets(s, SIZE, stdin) != NULL && s[0] != '\n')
+    {
+        fputs(s, stdout);
+    }
+
+    return 0;
+}
+
+void RemoveSpace_inString(char *s)
+{
+    while (s < s_end)
+    {
+        if (*s == ' ')
+        {
+            
+        }
+    }
+}
 
