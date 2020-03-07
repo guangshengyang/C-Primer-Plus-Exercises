a42a8a22d85c3c10ddb52c215451453a8ec4f61c
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-07 22:04:40 +0800

    A good structure to remove space in string
    void RemoveSpace_inString(char *s)
    {
        const char *d = s;
        do
        {
            while (isspace(*d))
            {
                ++d;
            }
    
        } while (*s++ = *d++);
    }

--- chapter11/Exercises 11.10.c
+++ chapter11/Exercises 11.10.c
@@ -6,10 +6,10 @@
 void RemoveSpace_inString(char *s);
 int main(void)
 {
-    char s[] = "I'm YangguangSheng from Hainan University.";
+    char s[];
     char *s_begin = s;
-    // while (fgets(s, SIZE, stdin) != NULL && s[0] != '\n')
-    //    fputs(s, stdout);
+
+    fgets(s, SIZE, stdin);
 
     RemoveSpace_inString(s);
     puts(s_begin);
@@ -19,7 +19,6 @@ int main(void)
 
 void RemoveSpace_inString(char *s)
 {
-    char *s_old = s;
     const char *d = s;
     do
     {
@@ -30,5 +29,3 @@ void RemoveSpace_inString(char *s)
 
     } while (*s++ = *d++);
 }
-/*https://stackoverflow.com/questions/1726302/removing-spaces-from-a-string-in-c*
-https://codescracker.com/c/program/c-program-remove-spaces-from-string.htm/
