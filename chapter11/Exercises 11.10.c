1dc383169fe9ea22c9dc4c42a84b32deb1de52eb
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-07 18:07:07 +0800

    Update Exercises 11.10.c

--- chapter11/Exercises 11.10.c
+++ chapter11/Exercises 11.10.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
-#define SIZE 30
+#include <stdbool.h>
+#define SIZE 50
 
 void RemoveSpace_inString(char *s);
 int main(void)
 {
-    char s[SIZE];
+    char s[] = "I'm YangguangSheng from Hainan University.";
+    char *s_begin = s;
+    // while (fgets(s, SIZE, stdin) != NULL && s[0] != '\n')
+    //    fputs(s, stdout);
 
-    while (fgets(s, SIZE, stdin) != NULL && s[0] != '\n')
-    {
-        fputs(s, stdout);
-    }
+    RemoveSpace_inString(s);
+    puts(s_begin);
 
     return 0;
 }
 
 void RemoveSpace_inString(char *s)
 {
-    while (s < s_end)
+    char *s_old = s;
+    const char *d = s;
+    do
     {
-        if (*s == ' ')
+        while (isspace(*d))
         {
-            
+            ++d;
         }
-    }
-}
 
+    } while (*s++ = *d++);
+}
+/*https://stackoverflow.com/questions/1726302/removing-spaces-from-a-string-in-c*
+https://codescracker.com/c/program/c-program-remove-spaces-from-string.htm/
