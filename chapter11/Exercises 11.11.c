9eb8d99feba0be03483306fa13e9c4d7de3da9ae
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-07 23:17:02 +0800

    Update Exercises 11.11.c

--- chapter11/Exercises 11.11.c
+++ chapter11/Exercises 11.11.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define SIZE 20
+void CheckStreamRead(void);
+int main(void)
+{
 
+    CheckStreamRead();
+
+    return 0;
+}
+
+void CheckStreamRead(void)
+{
+    char c;
+    int i = 0;
+    char buffer[SIZE];
+    while ((c = getchar()) != '\n')
+    {
+        buffer[i++] = c;
+    }
+    //buffer[i] = '\0';
+   // puts(buffer);
+    for (int j = 0; j < 10; j++)
+    {
+        printf("%c", buffer[j]);
+    }
+}
