c9bf4cc12ab7c0949579331711922ab0073bd3ae
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-11 01:12:57 +0800

    Update Exercises 11.12.c

--- chapter11/Exercises 11.12.c
+++ chapter11/Exercises 11.12.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#define SIZE 50
 
+void Report(char *s, int *storeresultary);
+
+int main(void)
+{
+    int i = 0;
+    char c;
+    char str[SIZE];
+    int storeresultary[6];
+
+    while ((c = getchar()) != EOF)
+        str[i++] = c;
+
+    if (str[--i] == '\n') // i++ make pointer point to next address
+        str[i] = '\0';    // , using --i to make it move back first, then do if Statement
+
+    Report(str,storeresultary);
+
+    return 0;
+}
+
+void Report(char *s, int *storeresultary)
+{
+    bool inword =false;
+
+    
+
+    
+}
