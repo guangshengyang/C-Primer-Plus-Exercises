e141817cd72c9dbbd99941a2f021816d7137516b
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-11 22:56:14 +0800

    Update Exercises 11.12.c

--- chapter11/Exercises 11.12.c
+++ chapter11/Exercises 11.12.c
@@ -18,16 +18,39 @@ int main(void)
     if (str[--i] == '\n') // i++ make pointer point to next address
         str[i] = '\0';    // , using --i to make it move back first, then do if Statement
 
-    Report(str,storeresultary);
+    Report(str, storeresultary);
 
     return 0;
 }
 
 void Report(char *s, int *storeresultary)
 {
-    bool inword =false;
+    bool inword = false;
+    int word_count = 0;
+    int ch_upper = 0;
+    int ch_lower = 0;
 
-    
+    // char c;
 
-    
+    while ((*s) != '\0')
+    {
+        if (!isspace(*s) && !inword) //c isn't blankspace and c isn't in word, so it's new word
+        {
+            inword = true;
+            word_count++;
+        }
+        if (isspace(*s) && inword)
+            inword = false;
+
+        if (isupper(*s))
+            ch_upper++;
+        if (islower(*s))
+            ch_lower++;
+        if (ispunct(*s))
+            ch_upper++;
+        if (isupper(*s))
+            ch_upper++;
+
+        s++;
+    }
 }
