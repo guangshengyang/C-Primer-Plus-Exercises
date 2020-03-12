2ba9b2b0dac5e0ddd074ab34d5709478520241dc
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-12 20:56:42 +0800

    Update Exercises 11.13.c

--- chapter11/Exercises 11.13.c
+++ chapter11/Exercises 11.13.c
@@ -1,5 +1,6 @@
-
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
 #define SIZE 50
 
@@ -8,30 +9,59 @@ int main(void)
     int i = 0;
     char c;
     char str[SIZE];
-    int store_start_end_ptr[SIZE];
+    char store_start_end_ptr[SIZE] = {0};
 
     while ((c = getchar()) != EOF) //store the characters as string-array type
         str[i++] = c;
 
     if (str[--i] == '\n') // i++ make pointer point to next address
         str[i] = '\0';    // , using --i to make it move back first, then do if Statement
+    SwapCmdOrder(str, store_start_end_ptr);
 
     return 0;
 }
-void SwapCmdOrder(char *s, int *store_start_end_ptr)
+void SwapCmdOrder(char *s, char *store_start_end_ptr)
 {
-    bool isfirst = true;
+    bool inword = false;
+
+    int i = 0, j = 0;
+    int n_word = 0;
+    int count = 0;
+    int wordlen[SIZE] = {0};
+
     while ((*s) != '\0')
     {
-        if (!isspace(*s) && !inword && isfirst) //c isn't blankspace and c isn't in word, so it's new word
+
+        if (!isspace(*s) && !inword) //c isn't blankspace and c isn't in word, so it's new word
         {
             inword = true;
-            isfirst = false;
-            store_start_end_ptr++ = s;
+            store_start_end_ptr[j++] = s;
+            n_word++;
         }
+
+        if (inword = true)
+        {
+            count++;
+        }
+        wordlen[i] = count;
         if (isspace(*s) && inword)
         {
+            count = 0;
             inword = false;
-            isfirst = true;
+            i++;
         }
+        s++;
     }
+    for (int j = 0; j < n_word; j++)
+    {
+        if (n_word == j + 1)
+            wordlen[j] += 1;
+
+        wordlen[j] -= 1;
+        printf("%d\t", wordlen[j]);
+        printf("%p\n", store_start_end_ptr[j]);
+    }
+    printf("\n\n");
+
+  
+}
