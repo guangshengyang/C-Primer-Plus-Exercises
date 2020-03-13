6ffd585a2e24eeb8e5e8213a1afe1d45919e6da4
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-13 17:28:10 +0800

    Update Exercises 11.14.c

--- chapter11/Exercises 11.14.c
+++ chapter11/Exercises 11.14.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+double calculate(double base, int index);
+
+int main(void)
+{
+    double base;
+    int index;
+    scanf("%lf %d", &base, &index);
+
+    printf("%lf", calculate(base, index));
+
+    return 0;
+}
+
+double calculate(double base, int index)
+{
+    double result = 1;
+    for (int i = 0; i < index; i++)
+        result *= base; //cumprod
+
+    return result;
+}
