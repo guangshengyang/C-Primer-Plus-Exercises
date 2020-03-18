f67424a4f27d448336708a68f3c7dcb1e08a6fa8
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-18 19:51:46 +0800

    Update Exercises 12.01.c

--- chapter12/Exercises 12.01.c
+++ chapter12/Exercises 12.01.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
 
-#include
+int critic(void);
+
+int main(void)
+{
+    int units;
+
+    printf("How many pound of a firkin of butter?\n");
+    scanf("%d", &units);
+
+    while (units != 56)
+        units = critic();
+    printf("You must have look it up!\n");
+
+    return 0;
+}
+
+int critic(void)
+{
+    int units;
+    printf("No luck, my friend. Try again.\n");
+    scanf("%d", &units);
+
+    return units; // return thr updated value for main() function
+}
