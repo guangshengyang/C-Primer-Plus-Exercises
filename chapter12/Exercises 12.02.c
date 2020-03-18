80f42e819b124b20f5f3d79bee01e8c2226cbd8b
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-18 21:57:17 +0800

    Update Exercises 12.02.c

--- chapter12/Exercises 12.02.c
+++ chapter12/Exercises 12.02.c
@@ -26,7 +26,83 @@ int main(void)
 
 /** pe12-2a.h */
 
+void set_mode(int mode);
+void get_info();
+void show_info();
+
+
 /** pe12-2a.h */
-28
+
+#include <stdio.h>
+
+int mode;
+double dist, fuel;
+
+void set_mode(int mode)
+{
+    int last_mode = mode;
+    if (mode != 0 || mode != 1)
+    {
+        if (last_mode == 0)
+            printf("Invaliad mode specified. Mode 0(metric) used");
+        if (last_mode == 1)
+            printf("Invaliad mode specified. Mode 1(US) used");
+    }
+}
+
+void get_info(void)
+{
+    extern int mode;
+    extern double dist;
+    extern double fuel;
+
+    switch (mode)
+    {
+    case 0:
+        printf("Enter distance traveled in kilometers:");
+        scanf("%lf", &dist);
+        printf("Enter fuel consumed in liters:");
+        scanf("%lf", &fuel);
+        break;
+    case 1:
+        printf("Enter distance traveled in niles:");
+        scanf("%lf", &dist);
+        printf("Enter fuel consumed in gallons:");
+        scanf("%lf", &fuel);
+        break;
+    case -1:
+        printf("Done.");
+        break;
+    default:
+        if (mode == 0)
+            printf("Invaliad mode specified. Mode 0(metric) used");
+        if (mode == 1)
+            printf("Invaliad mode specified. Mode 1(US) used");
+        break;
+    }
+}
+
+void show_info(void)
+{
+    extern int mode;
+    extern double dist;
+    extern double fuel;
+
+    double average;
+
+    switch (mode)
+    {
+    case 0:
+        average = fuel / dist / 100;
+        printf("Fuel consumption is %lf per 100 km.", average);
+        break;
+    case 1:
+        average = dist / fuel;
+        printf("Fuel consumption is %lf miles per gallon.", average);
+        break;
+    default:
+        break;
+    }
+}
 
 
