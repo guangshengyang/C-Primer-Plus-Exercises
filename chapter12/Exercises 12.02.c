539359dc50df2cab8c9fde5a68920377b5e7c8aa
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-19 10:26:26 +0800

    Update Exercises 12.02.c

--- chapter12/Exercises 12.02.c
+++ chapter12/Exercises 12.02.c
@@ -1,22 +1,32 @@
-/** pe12-2b.c */
+
+/* pe12-2b.c
+---------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include "pe12-2a.h"
 
 int main(void)
 {
     extern int mode;
+    extern int last_mode;
 
     printf("Enter 0 for metric mode, 1 for US mode:");
     scanf("%d", &mode);
 
     while (mode >= 0)
     {
-        set_mode(mode);
+        set_mode(); // If using set_mode(mode) rather set_mode()
+                    // This usage will redeclaration a block scope variable in set_mode() function
+                    // A new variable with the same name mode will be declared in the function definition section
+                    // with the same name with file scope(global scope) mode.
+                    // So when the function change value of mode, actually be changed variable is block scope mode, not file scope mode
+                    // Cause file scope mode is hidden by block scope mode(It's determined by the C standard)
+                    // When leave the set_mode function the block scope mode will disappear and the file scope mode value won't be hidden
+                    // So if you change the value in set_mode(mode) function, in fact it doesn't work
         get_info();
         show_info();
         printf("Enter 0 for metric mode, 1 for US mode"
                "(-1 to quit):");
-
         scanf("%d", &mode);
     }
     printf("Done.\n");
@@ -24,30 +34,42 @@ int main(void)
     return 0;
 }
 
-/** pe12-2a.h */
+/* pe12-2a.h
+---------------------------------------------------------------------- */
 
-void set_mode(int mode);
-void get_info();
-void show_info();
+void set_mode(void);
+void get_info(void);
+void show_info(void);
 
 
-/** pe12-2a.h */
+
+/* pe12-2a.c
+---------------------------------------------------------------------- */
 
 #include <stdio.h>
 
 int mode;
 double dist, fuel;
+int last_mode;
 
-void set_mode(int mode)
+void set_mode(void)
 {
-    int last_mode = mode;
-    if (mode != 0 || mode != 1)
+    extern int last_mode;
+
+    if (mode > 1)
     {
         if (last_mode == 0)
-            printf("Invaliad mode specified. Mode 0(metric) used");
+        {
+            printf("Invaliad mode specified. Mode 0(metric) used\n");
+            mode = 0;
+        }
         if (last_mode == 1)
-            printf("Invaliad mode specified. Mode 1(US) used");
+        {
+            printf("Invaliad mode specified. Mode 1(US) used\n");
+            mode = 1;
+        }
     }
+    last_mode = mode;
 }
 
 void get_info(void)
@@ -55,6 +77,7 @@ void get_info(void)
     extern int mode;
     extern double dist;
     extern double fuel;
+    extern int last_mode;
 
     switch (mode)
     {
@@ -74,10 +97,6 @@ void get_info(void)
         printf("Done.");
         break;
     default:
-        if (mode == 0)
-            printf("Invaliad mode specified. Mode 0(metric) used");
-        if (mode == 1)
-            printf("Invaliad mode specified. Mode 1(US) used");
         break;
     }
 }
@@ -93,16 +112,14 @@ void show_info(void)
     switch (mode)
     {
     case 0:
-        average = fuel / dist / 100;
-        printf("Fuel consumption is %lf per 100 km.", average);
+        average = (fuel / dist) * 100;
+        printf("Fuel consumption is %lf per 100 km.\n", average);
         break;
     case 1:
         average = dist / fuel;
-        printf("Fuel consumption is %lf miles per gallon.", average);
+        printf("Fuel consumption is %lf miles per gallon.\n", average);
         break;
     default:
         break;
     }
 }
-
-
