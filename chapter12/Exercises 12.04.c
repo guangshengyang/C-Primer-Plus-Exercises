0823d6f7c21bddd2b511da05e19b2bc8b03e15de
Guang Sheng Yang <60422898+guangshengyang@users.noreply.github.com>
2020-03-20 22:32:37 +0800

    Update Exercises 12.04.c

--- chapter12/Exercises 12.04.c
+++ chapter12/Exercises 12.04.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int g_calltimes = 0;
+
+void return_call_times(void);
+
+int main(void)
+{
+    for (int i = 0; i < 50; i++)
+        return_call_times();
+
+    printf("Call times:%d", g_calltimes);
+
+    return 0;
+}
+
+void return_call_times()
+{
+    extern int g_calltimes;
+    g_calltimes++;
+}
