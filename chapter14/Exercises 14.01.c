137ce97bd800f5fbb46900f688bf43d8a67e31ca
guangshengyang <guangshengyang01@gmail.com>
2020-04-14 23:55:05 +0800

    Create Exercises 01

--- chapter14/Exercises 14.01.c
+++ chapter14/Exercises 14.01.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#define SIZE 10
+#define LENGTH 3
 
+struct Days
+{
+    char months[SIZE];
+    char months_abbreviation[LENGTH];
+    int months_days;
+    int months_num;
+
+
+};
+
+int main(void)
+{
+
+    return 0;
+}
