13fef91cc0e8ccbbfa9f8add896eb79558e71df2
guangshengyang <guangshengyang01@gmail.com>
2020-04-15 17:03:31 +0800

    commit

--- chapter14/Exercises 14.01.c
+++ chapter14/Exercises 14.01.c
@@ -5,17 +5,63 @@
 
 struct Days
 {
-    char months[SIZE];
-    char months_abbreviation[LENGTH];
+    char months_name[SIZE];
+    char months_name_abbreviation[LENGTH];
     int months_days;
-    int months_num;
+    int months_Serial_num;
 };
 
-struct Days array[MONTHS];
+struct Days months_array[MONTHS] = {
+    {"January", "Jan", 31, 1},
+    {"February", "Feb", 28, 2},
+    {"March", "Mar", 31, 3},
+    {"April", "Apr", 30, 4},
+    {"May", "May", 31, 5},
+    {"June ", "Jun", 30, 6},
+    {"July", "Jul", 31, 7},
+    {"August", "Aug", 31, 8},
+    {"September", "Sep", 30, 9},
+    {"October", "Oct", 31, 10},
+    {"November", "Nov", 30, 11},
+    {"December", "Dec", 31, 12}};
+
+char *s_gets(char *s, int n);
 
 int main(void)
 {
-    
+    char monthsname[SIZE];
+    printf("Enter the number of the months name(abbreviation is OK, too)\n"
+           "I'll count the how many days till this month(include this month):");
+    s_gets(monthsname, SIZE);
+    count_days();
 
     return 0;
+}
+
+int count_days()
+{
+    extern struct Days months_array[MONTHS];
+    
+}
+
+char *s_gets(char *s, int n)
+{
+    char *ret_val;
+    int i = 0;
+
+    ret_val = fgets(s, n, stdin);
+    if (ret_val)
+    {
+        while (s[i] != '\n' && s[i] != '\0')
+            s++;
+        if (s[i] == '\n')
+            s[i] = '\0';
+        else
+        {
+            while (getchar() != '\n')
+                continue;
+        }
+    }
+
+    return ret_val;
 }
